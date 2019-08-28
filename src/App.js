e9b4b683253f91d8b3f0aca9bebb3298866a67dd
sveta <svetlana.shurla4acova@gmail.com>
2019-08-28 14:22:01 +0300

    added redux, implemented userReducer and userAction to Header and App components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,46 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import './App.css';
+import {connect} from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
+import {setCurrentUser} from './redux/user/user.actions';
+import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   onsubscribeFromAuth = null;
 
   componentDidMount() {
     //подписываемся на изменения (логирование, разлогирование, регистрация)
     this.onsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      const {setCurrentUser} = this.props;
+
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         })
       } else {
-        this.setState({currentUser: userAuth});
+        setCurrentUser(userAuth);
       }
     })
   }
 
   componentWillUnmount() {
+    //отписка
+    //onAuthStateChanged возвращает функцию, вызвав которую мы удаляем слушатель событий авторизации
     this.onsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route path="/shop" component={ShopPage}/>
@@ -54,4 +51,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
