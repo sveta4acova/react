c60ee6b2c89097c5d1906fbbc4ecee485b5e1b72
sveta <svetlana.shurla4acova@gmail.com>
2020-01-20 14:33:21 +0300

    small edit

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ const app = express();
 const port  = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
+
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
